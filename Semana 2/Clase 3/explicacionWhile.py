8822ebce98b008f16a9a634d50b57578ed6f6ddd
Daniel Saldarriaga <dsaldarriaga@prevalentware.com>
2021-05-27 06:45:52 -0500

    agregar primer ejercicio de while, semana 2 clase 3

---
+++ Semana 2/Clase 3/explicacionWhile.py
@@ -0,0 +1,12 @@
+
+numero = 0
+
+while numero != 50:
+    numero = int(input("por favor ingrese un numero: "))
+
+# while True:
+#     print("hola, soy un ciclo que nunca acaba 1")
+#     print("hola, soy un ciclo que nunca acaba 2")
+#     print("hola, soy un ciclo que nunca acaba 3")
+#     print("hola, soy un ciclo que nunca acaba 4")
+#     print("hola, soy un ciclo que nunca acaba 5")
---
+++ Semana 2/Clase 3/juegoAdivinanza.py
@@ -0,0 +1,11 @@
+numeroIngresadoPorElUsuario = int(input("Adivina cuál número está registrado como varibale: "))
+numeroAEncontrar = 7
+
+while numeroIngresadoPorElUsuario != numeroAEncontrar:
+    numeroIngresadoPorElUsuario = int(input("Adivina cuál número está registrado como varibale: "))
+    if numeroIngresadoPorElUsuario > numeroAEncontrar:
+        print("El numero que estás buscando es menor al que ingresaste")
+    elif numeroIngresadoPorElUsuario < numeroAEncontrar:
+        print("El número que estás buscando es mayor al que ingresaste")
+
+print("¡Correcto! El número que ingresaste es el correcto.")
