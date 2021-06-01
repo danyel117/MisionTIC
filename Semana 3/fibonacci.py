fe3ab0bfa9c7fcf1d4f5147c90ee8d9cf6275558
Daniel Saldarriaga <dsaldarriaga@prevalentware.com>
2021-06-01 06:17:50 -0500

    agregar ejercicio de fibonacci, semana 3 clase 1

---
+++ Semana 3/fibonacci.py
@@ -0,0 +1,11 @@
+#Write a Python program to get the Fibonacci series between 0 to 50. 
+# The Fibonacci Sequence is the series of numbers : 0, 1, 1, 2, 3, 5, 8, 13, 21
+
+listFibonacci = [0,1]
+
+cont = 1
+while len(listFibonacci)<50:
+    listFibonacci.append(listFibonacci[cont] + listFibonacci[cont-1])
+
+
+print(listFibonacci)
