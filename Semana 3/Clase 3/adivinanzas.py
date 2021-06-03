9aacbf5a15edb7f3cf17ae9f5782d48e6710a10a
Daniel Saldarriaga <dsaldarriaga@prevalentware.com>
2021-06-03 06:36:06 -0500

    agregar funcion para el juego de las adivinanzas

---
+++ Semana 3/Clase 3/adivinanzas.py
@@ -0,0 +1,46 @@
+#Hacer un juego de adivinanzas
+#el número a adivinar es un aleatorio
+# entre 0 y 20
+# el código debe genear automáticamente el aleatorio
+# y preguntarle al usuario con un input el numero que quiere adivinar
+# el juego debe tener un máximo número de intentos.
+# si me paso del máximo sin adivinar, pierdo
+# si adivino, gano
+
+import random
+
+def GuessTheNumber(maxRetries):
+    #numero a adivinar
+    numberToGuess = random.randint(0,20)
+
+    #numero maximo de intentos
+    maxGuess = maxRetries
+
+    #contador de intentos del usuario
+    contUserGuess = 0
+
+    #variable para determinar si un usuario ganó
+    won = False
+
+    while contUserGuess < maxGuess and not won:
+        userInput = int(input("Adivina el número entre 0 y 20: "))
+        if userInput == numberToGuess:
+            print("¡Has ganado!")
+            won = True
+            return won
+        else:
+            print("Respuesta Incorrecta")
+            contUserGuess = contUserGuess + 1
+            print(f"Te quedan {maxGuess - contUserGuess} intentos")
+
+    if contUserGuess == maxGuess:
+        print(f"¡Has perdido! El número a encontrar era {numberToGuess}")
+    
+    return won
+
+result = GuessTheNumber(5)
+print(result)
+
+
+
+
