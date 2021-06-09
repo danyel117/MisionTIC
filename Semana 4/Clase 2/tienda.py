22661ab52ad621bb53a16d65215d6e57bbff4a2e
Daniel Saldarriaga <dsaldarriaga@prevalentware.com>
2021-06-09 07:22:19 -0500

    agregar método para agregar un producto a la clase tienda

--- Semana 4/Clase 2/tienda.py
+++ Semana 4/Clase 2/tienda.py
@@ -1,3 +1,5 @@
+from producto import Producto
+
 #1:
 class Tienda:
     #2:
@@ -6,6 +8,9 @@ class Tienda:
         self.paginaWeb = paginaWeb
         self.direccion = direccion
         self.listaDeProductos = []
+    #5:
+    def agregarProducto(self,productoAAgregar):
+        self.listaDeProductos.append(productoAAgregar)
 
 
 #3:
@@ -13,6 +18,11 @@ nombreTienda = input("Ingrese el nombre de la tienda a crear: ")
 paginaWeb = input("Ingrese la página web de la tienda: ")
 direccion = input("Ingrese la dirección de la tienda: ")
 tienda = Tienda(nombreTienda,paginaWeb,direccion)
+
+#6:
+zapatos = Producto("zapatos",50000)
+tienda.agregarProducto(zapatos)
+print(tienda.listaDeProductos)
 
 
 
