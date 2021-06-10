41dafec1ee2e5a992b09b39c54242d10f3a88582
Daniel Saldarriaga <dsaldarriaga@prevalentware.com>
2021-06-10 06:55:03 -0500

    agregar archivo main.py para ejecutar la lógica de la aplicación. Clase 3, semana 4

---
+++ Semana 4/Clase 3/main.py
@@ -0,0 +1,44 @@
+#10:
+from tienda import Tienda
+from producto import Producto
+
+
+#3:
+nombreTienda = input("Ingrese el nombre de la tienda a crear: ")
+paginaWeb = input("Ingrese la página web de la tienda: ")
+direccion = input("Ingrese la dirección de la tienda: ")
+tienda = Tienda(nombreTienda,paginaWeb,direccion)
+
+#7:
+while True:
+    instrucciones = """
+        Ingrese NP para crear un nuevo producto, 
+        I para imprimir los productos e inventarios
+        V para ejecutar una venta
+    """
+    operacion = input(instrucciones)
+    if operacion == "NP":
+        #6:
+        nombre = input("Ingrese el nombre del producto: ")
+        precio = int(input("Ingrese el precio del producto: "))
+        productoCreado = Producto(nombre,precio)
+        tienda.agregarProducto(productoCreado)
+    elif operacion == "I":
+        tienda.imprimirProductosEInventarios()
+    elif operacion == "V":
+        #9:
+        nombreProducto = input("Ingrese el nombre del producto que quiere comprar: ")
+        productoEncontrado = tienda.buscarProductoPorNombre(nombreProducto)
+        if not productoEncontrado:
+            print("El producto no fue encontrado")
+        else:
+            cantidadAComprar = int(input("Ingrese la cantidad a comprar: "))
+            if productoEncontrado.inventario >= cantidadAComprar:
+                total = productoEncontrado.precio * cantidadAComprar
+                print(f"Venta Exitosa, el total de la venta es: ${total} pesos")
+                productoEncontrado.inventario = productoEncontrado.inventario - cantidadAComprar
+                #productoEncontrado.inventario -= cantidadAComprar
+            else:
+                print("No hay inventario suficiente de la referencia que desea comprar")
+
+
--- Semana 4/Clase 3/tienda.py
+++ Semana 4/Clase 3/tienda.py
@@ -18,44 +18,9 @@ class Tienda:
             print("Inventario: ",producto.inventario)
             print("___________")
     
+    #9:
     def buscarProductoPorNombre(self,nombreProductoABuscar):
         for producto in self.listaDeProductos:
             if producto.nombre == nombreProductoABuscar:
                 return producto
         return False
-
-
-
-#3:
-nombreTienda = input("Ingrese el nombre de la tienda a crear: ")
-paginaWeb = input("Ingrese la página web de la tienda: ")
-direccion = input("Ingrese la dirección de la tienda: ")
-tienda = Tienda(nombreTienda,paginaWeb,direccion)
-
-
-#7:
-while True:
-    operacion = input("Ingrese NP para crear un nuevo producto, I para imprimir los productos e inventarios: ")
-    if operacion == "NP":
-        #6:
-        nombre = input("Ingrese el nombre del producto: ")
-        precio = int(input("Ingrese el precio del producto: "))
-        productoCreado = Producto(nombre,precio)
-        tienda.agregarProducto(productoCreado)
-    elif operacion == "I":
-        tienda.imprimirProductosEInventarios()
-    elif operacion == "V":
-        nombreProducto = input("Ingrese el nombre del producto que quiere comprar: ")
-        productoEncontrado = tienda.buscarProductoPorNombre(nombreProducto)
-        if not productoEncontrado:
-            print("El producto no fue encontrado")
-        else:
-            cantidadAComprar = int(input("Ingrese la cantidad a comprar: "))
-            if productoEncontrado.inventario >= cantidadAComprar:
-                total = productoEncontrado.precio * cantidadAComprar
-                print(f"Venta Exitosa, el total de la venta es: ${total} pesos")
-                productoEncontrado.inventario -= cantidadAComprar
-            else:
-                print("No hay inventario suficiente de la referencia que desea comprar")
-
-
