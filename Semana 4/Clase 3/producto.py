013e48e5c66e3ea83d9ac70b5e2115d683a11b36
Daniel Saldarriaga <dsaldarriaga@prevalentware.com>
2021-06-11 07:03:57 -0500

    agregar ejercicio final semana 4

--- Semana 4/Clase 3/producto.py
+++
@@ -1,8 +0,0 @@
-import random
-#4:
-class Producto:
-    def __init__(self,nombre,precio):
-        self.nombre = nombre
-        self.precio = precio
-        self.referencia = "REF"+str(random.randint(1000,10000))
-        self.inventario = random.randint(0,100)
