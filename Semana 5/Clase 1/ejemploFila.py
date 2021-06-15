a6e645b07eedd4b0b5adf2a7d1be49a16a034b85
Daniel Saldarriaga <dsaldarriaga@prevalentware.com>
2021-06-15 07:02:31 -0500

    agregar instrucciones para el ejericio de la clínica

---
+++ Semana 5/Clase 1/ejemploFila.py
@@ -0,0 +1,17 @@
+#implementar un sistema de turnos en una clínica
+
+#crear una clase "Clinica" que tenga como atributos un nombre
+# y una lista de pacientes
+
+#crear una clase Paciente que tenga los atributos nombre, documento y turno
+
+#la clase Clinica tiene un método para ingresar un nuevo paciente. El paciente
+#que ingresa, debe ingresar al final de la fila.
+
+#la clase clínica tiene un método para "atender" un nuevo paciente. Se debe
+#atender primero al primer paciente que ingresa.
+
+#la clínica solo puede tener 10 pacientes en espera
+#cuando ingresa un nuevo paciente, se debe verificar que haya espacio.
+
+#si ya hay 10 pacientes en espera, se debe sacar un mensaje de error.
