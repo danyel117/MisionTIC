14414644a6db92e52740c6966ab1865472151fab
Daniel Saldarriaga <dsaldarriaga@prevalentware.com>
2021-06-22 07:44:23 -0500

    agregar instrucciones de clase tarea

---
+++ Semana 6/Clase 1/tareas.py
@@ -0,0 +1,18 @@
+#implementar un sistema de gestión de tareas
+#un usuario debe tener un nombre, una lista de tareas y una tarea en curso
+#los elementos de la lista de tareas son de una clase Tarea
+#la clase Tarea tiene los siguientes atributos:
+    #descripción de la tarea
+    #fecha límite de la tarea
+#la clase usuario tiene los siguientes atributos:
+    #nombre
+    #tareaActual
+    #listaTareas
+#la clase usuario tiene los siguientes métodos:
+    #agregar una tarea:
+        #si el usuario no tiene tarea actual, la agrega como tarea actual
+        #si el usuario ya tiene tarea actual, la agrega a la lista de tareas pendientes
+    #finalizar una tarea
+        #saca la siguiente tarea de la lista y la pone en "tarea actual"
+
+#1. hacer una clase Tarea
