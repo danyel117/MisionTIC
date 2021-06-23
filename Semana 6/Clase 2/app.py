1aa543c7cde998c7eab6d2a1593cf148368e9582
Daniel Saldarriaga <dsaldarriaga@prevalentware.com>
2021-06-23 07:34:48 -0500

    agregar aplicación de Flask, ruta de index y template index.html

---
+++ Semana 6/Clase 2/app.py
@@ -0,0 +1,45 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+class Task:
+    def __init__(self,description, dueDate):
+        self.description = description
+        self.dueDate = dueDate
+
+    def __str__(self):
+        return self.description + " - " + self.dueDate
+class User:
+    def __init__(self,name):
+        self.name=name
+        self.currentTask = ''
+        self.taskList = []
+
+    def addTask(self,newTask):
+        #revisar si el usuario ya tiene una "tarea actual"
+        if self.currentTask == '':
+            self.currentTask = newTask
+        else:
+            self.taskList.append(newTask)
+
+    def endTask(self):
+        if len(self.taskList)>0:
+            self.currentTask = self.taskList.pop(0)
+        else:
+            self.currentTask = ''
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+@app.route("/ruta2")
+def ruta2():
+    return "hola, soy la ruta 2"
+
+@app.route("/usuario")
+def usuario():
+    return "hola, ruta de usuarios"
+
+
+if __name__=="__main__":
+    app.run(debug=True)
