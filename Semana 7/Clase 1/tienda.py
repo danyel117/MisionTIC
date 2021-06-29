ea9958b33d03c0d86cb8c9d05d0c9717f8f09520
Daniel Saldarriaga <dsaldarriaga@prevalentware.com>
2021-06-29 06:11:48 -0500

    generar instrucciones de clase tienda. Clase 1, semana 7

---
+++ Semana 7/Clase 1/tienda.py
@@ -0,0 +1,21 @@
+#1. crear una clase Tienda que tenga los siguientes atributos:
+    #nombre
+    #direccion
+    #telefono
+    #lista de productos
+    #lista de clientes
+    #histórico de ventas
+
+#2. crear una clase Producto que tenga los siguientes atributos:
+    #nombre
+    #precio
+
+#3. crear una clase Cliente que tenga los siguientes atributos:
+    #nombre
+    #documento
+    #lista de compras
+
+#4. crear una clase Venta que tenga los siguientes atributos:
+    #fecha
+    #lista de productos y cantidades
+    #cliente
