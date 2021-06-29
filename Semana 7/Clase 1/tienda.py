b80dfbe5b8b575c6353fc7d742d60d353ab9d0ae
Daniel Saldarriaga <dsaldarriaga@prevalentware.com>
2021-06-29 06:35:46 -0500

    agregar metodo para agregar un producto a la clase tienda. Clase 1, semana 7

--- Semana 7/Clase 1/tienda.py
+++ Semana 7/Clase 1/tienda.py
@@ -13,15 +13,25 @@ class Tienda:
         self.productos=[]
         self.clientes=[]
         self.ventas=[]
+    #definir un método para agregar un nuevo producto
+    def agregarProducto(self,producto):
+        self.productos.append(producto)
+    
+    def imprimirProductos(self):
+        for producto in self.productos:
+            print(producto)
+
 
 #2. crear una clase Producto que tenga los siguientes atributos:
     #nombre
     #precio
-
 class Producto:
     def __init__(self,nombre, precio):
         self.nombre=nombre
         self.precio=precio
+    
+    def __str__(self):
+        return self.nombre + " - " + str(self.precio)
 
 #3. crear una clase Cliente que tenga los siguientes atributos:
     #nombre
@@ -32,6 +42,7 @@ class Cliente:
         self.nombre=nombre
         self.documento=documento
         self.compras=[]
+    
 
 #4. crear una clase Venta que tenga los siguientes atributos:
     #fecha
@@ -42,3 +53,21 @@ class Venta:
         self.cliente=cliente
         self.fecha=fecha
         self.productos=[]
+
+
+#crear la lógica de la aplicación
+tienda = Tienda("Tienda MisionTIC","calle 123","321")
+
+while True:
+    instrucciones="""
+        ingrese P para agregar un nuevo producto a la tienda
+        ingrese IP para imprimir los productos de la tienda
+    """
+    operacion = input(instrucciones)
+    if operacion == "P":
+        nombreProducto=input("ingrese el nombre del producto: ")
+        precioProducto=float(input("Ingrese el precio del producto: "))
+        nuevoProducto = Producto(nombreProducto,precioProducto)
+        tienda.agregarProducto(nuevoProducto)
+    elif operacion == "IP":
+        tienda.imprimirProductos()
