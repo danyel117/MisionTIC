8cfdfb4041a39d8f34e5b431cfee050aed2b7066
Daniel Saldarriaga <dsaldarriaga@prevalentware.com>
2021-06-29 06:21:31 -0500

    agregar clases tienda, productos, cliente y ventas

--- Semana 7/Clase 1/tienda.py
+++ Semana 7/Clase 1/tienda.py
@@ -5,17 +5,40 @@
     #lista de productos
     #lista de clientes
     #histórico de ventas
+class Tienda:
+    def __init__(self, nombre,direccion,telefono):
+        self.nombre=nombre
+        self.direccion=direccion
+        self.telefono=telefono
+        self.productos=[]
+        self.clientes=[]
+        self.ventas=[]
 
 #2. crear una clase Producto que tenga los siguientes atributos:
     #nombre
     #precio
 
+class Producto:
+    def __init__(self,nombre, precio):
+        self.nombre=nombre
+        self.precio=precio
+
 #3. crear una clase Cliente que tenga los siguientes atributos:
     #nombre
     #documento
     #lista de compras
+class Cliente:
+    def __init__(self,nombre,documento):
+        self.nombre=nombre
+        self.documento=documento
+        self.compras=[]
 
 #4. crear una clase Venta que tenga los siguientes atributos:
     #fecha
     #lista de productos y cantidades
     #cliente
+class Venta:
+    def __init__(self,cliente,fecha):
+        self.cliente=cliente
+        self.fecha=fecha
+        self.productos=[]
